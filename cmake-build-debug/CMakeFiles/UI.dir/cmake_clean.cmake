b44b094144a29e2a2d0e589ed1fa9a798c69cb95
Xx220xX <hslhenriquegoscara@gmail.com>
2021-07-25 10:01:25 -0300

    init project

---
+++ cmake-build-debug/CMakeFiles/UI.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UI.dir/extern/include/nuklear/nuklear.c.obj"
+  "CMakeFiles/UI.dir/main.c.obj"
+  "UI.exe"
+  "UI.exe.manifest"
+  "UI.pdb"
+  "libUI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
